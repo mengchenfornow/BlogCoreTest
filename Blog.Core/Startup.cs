00edcd0ad86d4a5c9198e421ceaaae406bfc3523
陈庭钊 <chentingzhao@outlook.com>
2018-10-27 15:59:22 +0800

    添加README.md

---
+++ Blog.Core/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Blog.Core
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            #region swagger
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info()
+                {
+                    Version = "v0.1.0",
+                    Title = "Blog.Core.Api",
+                    Description = "框架文档说明",
+                    TermsOfService = "None",
+                    Contact = new Swashbuckle.AspNetCore.Swagger.Contact()
+                    {
+                        Email = "Blog.Core@qq.com",
+                        Name = "Blog.Core",
+                        Url = "https://www.jianshu.com/u/94102b59cc2a"
+                    }
+                });
+            });
+            #endregion
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            //呵呵呵
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            #region swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiHelp V1");
+                s.RoutePrefix = "";
+            });
+            #endregion
+        }
+    }
+}
